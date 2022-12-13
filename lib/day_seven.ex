f18b0b20b4b6debc5e4033ef5892a8c11156cde3
Tiago Avila <tiago.avila09@gmail.com>
2022-12-13 19:02:47 -0300

    WIP day 7

--- lib/day_seven.ex
+++ lib/day_seven.ex
@@ -1,16 +1,29 @@
 defmodule DaySeven do
-	def get_total_size_of_directories_at_most_100k(input) do
+  def get_total_size_of_directories_at_most_100k(input) do
     input
     |> String.split("\n", trim: true)
+    |> Enum.map(&get_file_sizes/1)
+    |> Enum.filter(&is_size_less_than_100k?/1)
+    |> Enum.sum()
+  end
+
+  defp get_file_sizes("$" <> _rest) do
+    0
+  end
+
+  defp get_file_sizes("dir" <> _rest) do
+    0
+  end
+
+  defp get_file_sizes(line) do
+    [size | _rest] =
+      line
+      |> String.split(" ", trim: true)
+
+    size
+    |> String.to_integer()
     |> IO.inspect
-    |> Enum.map(&process_line/1)
-	end
-
-  defp process_line(<<codepoint::utf8, rest::binary>> = line) when codepoint == ?$ do
-1
   end
 
-  defp process_line(line) do
-    2
-  end
+  defp is_size_less_than_100k?(file_size), do: file_size < 100000
 end
