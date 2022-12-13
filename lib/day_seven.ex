909b110cbb69118e3e8457006739b696f6d21d88
Tiago Avila <tiago.avila09@gmail.com>
2022-12-13 19:40:01 -0300

    WIP improvements for day 7

--- lib/day_seven.ex
+++ lib/day_seven.ex
@@ -1,18 +1,41 @@
 defmodule DaySeven do
+  defstruct parent_dirs: [], current_dir: "/", dirs: %{}
+
   def get_total_size_of_directories_at_most_100k(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&get_file_sizes/1)
-    |> Enum.filter(&is_size_less_than_100k?/1)
-    |> Enum.sum()
+    |> Enum.reduce(%DaySeven{}, &process_line/2)
+#    |> Enum.filter(&is_size_less_than_100k?/1)
+#    |> Enum.sum()
   end
 
-  defp get_file_sizes("$" <> _rest) do
-    0
+  defp process_line("$ cd /", acc) do
+    acc
   end
 
-  defp get_file_sizes("dir" <> _rest) do
-    0
+  defp process_line("$ ls", acc) do
+    acc
+  end
+
+  defp process_line("dir " <> _rest, acc) do
+    acc
+  end
+
+  defp process_line("$ cd ..", %DaySeven{parent_dirs: [latest_parent | remaining_parents]} = acc) do
+    %{acc | current_dir: latest_parent, parent_dirs: remaining_parents}
+  end
+
+  defp process_line("$ cd " <> dir_name, %DaySeven{current_dir: "/"} = acc) do
+    %{acc | current_dir: dir_name, parent_dirs: ["/"]}
+  end
+
+  defp process_line("$ cd " <> dir_name, %DaySeven{current_dir: new_parent_dir} = acc) do
+    %{acc | current_dir: dir_name, parent_dirs: [new_parent_dir | acc.parent_dirs]}
+  end
+
+  defp process_line(file_line, acc) do
+    acc
+    |> IO.inspect
   end
 
   defp get_file_sizes(line) do
