a8771a6cecc108dfc66f36f9a12c148f98052987
Tiago Avila <tiago.avila09@gmail.com>
2022-12-14 11:43:02 -0300

    Still working on day 7

--- lib/day_seven.ex
+++ lib/day_seven.ex
@@ -1,52 +1,54 @@
 defmodule DaySeven do
-  defstruct parent_dirs: [], current_dir: "/", dirs: %{}
+  defstruct parent_dirs: [], current_dir: "/", dirs: %{"/" => 0}
 
   def get_total_size_of_directories_at_most_100k(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.reduce(%DaySeven{}, &process_line/2)
-#    |> Enum.filter(&is_size_less_than_100k?/1)
-#    |> Enum.sum()
+    |> then(fn %DaySeven{dirs: dirs} -> dirs |> Map.values() end)
+    |> Enum.filter(&is_size_less_than_100k?/1)
+    |> Enum.sum()
   end
 
-  defp process_line("$ cd /", acc) do
-    acc
-  end
-
-  defp process_line("$ ls", acc) do
-    acc
-  end
-
-  defp process_line("dir " <> _rest, acc) do
-    acc
-  end
+  defp process_line("$ cd /", acc), do: acc
+  defp process_line("$ ls", acc), do: acc
+  defp process_line("dir " <> _rest, acc), do: acc
 
   defp process_line("$ cd ..", %DaySeven{parent_dirs: [latest_parent | remaining_parents]} = acc) do
     %{acc | current_dir: latest_parent, parent_dirs: remaining_parents}
   end
 
-  defp process_line("$ cd " <> dir_name, %DaySeven{current_dir: "/"} = acc) do
-    %{acc | current_dir: dir_name, parent_dirs: ["/"]}
-  end
-
   defp process_line("$ cd " <> dir_name, %DaySeven{current_dir: new_parent_dir} = acc) do
-    %{acc | current_dir: dir_name, parent_dirs: [new_parent_dir | acc.parent_dirs]}
+    updated_dirs = Map.put_new(acc.dirs, dir_name, 0)
+    %{acc | current_dir: dir_name, parent_dirs: [new_parent_dir | acc.parent_dirs], dirs: updated_dirs}
   end
 
   defp process_line(file_line, acc) do
-    acc
-    |> IO.inspect
-  end
-
-  defp get_file_sizes(line) do
-    [size | _rest] =
-      line
+    [size | _] =
+      file_line
       |> String.split(" ", trim: true)
 
     size
-    |> String.to_integer()
-    |> IO.inspect
+    |> Integer.parse()
+    |> process_file_size(acc)
   end
 
-  defp is_size_less_than_100k?(file_size), do: file_size < 100000
+  defp process_file_size(:error, acc), do: acc
+
+  defp process_file_size({file_size, _}, %DaySeven{current_dir: current_dir} = acc) do
+    updated_dirs_with_current_file =
+      acc.dirs
+      |> Map.update!(current_dir, fn existing_value -> existing_value + file_size end)
+
+    updated_dirs =
+      acc.parent_dirs
+      |> Enum.reduce(updated_dirs_with_current_file, fn parent, d_acc ->
+        d_acc
+        |> Map.update!(parent, fn existing_value -> existing_value + file_size end)
+      end)
+
+    %{acc | dirs: updated_dirs}
+  end
+
+  defp is_size_less_than_100k?(file_size), do: file_size <= 100_000
 end
--- test/day_seven_test.exs
+++ test/day_seven_test.exs
@@ -7,12 +7,12 @@ defmodule DaySevenTest do
            |> DaySeven.get_total_size_of_directories_at_most_100k() == 95437
   end
 
-  #  test "Day 07 - get result for challenge input part 1" do
-  #    File.read!("./inputs/day_seven/challenge_input.txt")
-  #    |> DaySeven.get_crates_on_top()
-  #    |> IO.inspect(label: "Day 07 - Part 01: result for challenge input part 1 is")
-  #  end
-  #
+  test "Day 07 - get result for challenge input part 1" do
+    File.read!("./inputs/day_seven/challenge_input.txt")
+    |> DaySeven.get_total_size_of_directories_at_most_100k()
+    |> IO.inspect(label: "Day 07 - Part 01: result for challenge input part 1 is")
+  end
+
   #
   #  test "Day 07 Part 2 - for test input crates on top are MCD" do
   #    assert File.read!("./inputs/day_seven/test_input.txt")
