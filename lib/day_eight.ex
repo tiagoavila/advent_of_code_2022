6da3065442c8aebb3d0970125bfb6c1c9a49dbe9
Tiago Avila <tiago.avila09@gmail.com>
2022-12-27 10:38:46 -0300

    day 08 part 1

--- lib/day_eight.ex
+++ lib/day_eight.ex
@@ -33,14 +33,51 @@ defmodule DayEight do
     |> Enum.reverse()
   end
 
-  defp tree_is_visible?(_, row, col, _, grid_length) when row == 0 or col == 0 or row == grid_length - 1 or col == grid_length - 1 do
+  defp tree_is_visible?(_, row, col, _, grid_length)
+       when row == 0 or col == 0 or row == grid_length - 1 or col == grid_length - 1 do
     true
   end
 
-  defp tree_is_visible?(grid, tree_row, tree_col, tree_height, grid_length) do
-    grid
-    |> Enum.filter(fn {row, _, _} -> row == tree_row end)
-    |> IO.inspect()
+  defp tree_is_visible?(grid, tree_index_row, tree_index_col, tree_height, grid_length) do
+    row =
+      grid
+      |> Enum.filter(fn {row_index, _, _} -> row_index == tree_index_row end)
 
+    col =
+      grid
+      |> Enum.filter(fn {_, col_index, _} -> col_index == tree_index_col end)
+
+    with false <- tree_is_visible_from_left?(row, tree_index_col, tree_height, grid_length),
+         false <- tree_is_visible_from_right?(row, tree_index_col, tree_height, grid_length),
+         false <- tree_is_visible_from_top?(col, tree_index_row, tree_height, grid_length),
+         false <- tree_is_visible_from_bottom?(col, tree_index_row, tree_height, grid_length) do
+      false
+    else
+      _ -> true
+    end
+  end
+
+  defp tree_is_visible_from_left?(row, col_index, tree_height, grid_length) do
+    row
+    |> Enum.slice(0, col_index)
+    |> Enum.all?(fn {_, _, height} -> height < tree_height end)
+  end
+
+  defp tree_is_visible_from_right?(row, col_index, tree_height, grid_length) do
+    row
+    |> Enum.slice(col_index + 1, length(row) - col_index)
+    |> Enum.all?(fn {_, _, height} -> height < tree_height end)
+  end
+
+  defp tree_is_visible_from_top?(col, row_index, tree_height, grid_length) do
+    col
+    |> Enum.slice(0, row_index)
+    |> Enum.all?(fn {_, _, height} -> height < tree_height end)
+  end
+
+  defp tree_is_visible_from_bottom?(col, row_index, tree_height, grid_length) do
+    col
+    |> Enum.slice(row_index + 1, length(col) - row_index)
+    |> Enum.all?(fn {_, _, height} -> height < tree_height end)
   end
 end
--- test/day_eight_test.exs
+++ test/day_eight_test.exs
@@ -2,17 +2,17 @@ defmodule DayEightTest do
   use ExUnit.Case
   doctest DayEight
 
-  test "Day 08 - for test input the sum of the total sizes of the directories is 95438" do
+  test "Day 08 - for test input the number of trees that are visible from outside the grid is 21" do
     assert File.read!("./inputs/day_eight/test_input.txt")
            |> DayEight.get_number_of_visible_trees() == 21
   end
 
-#  test "Day 08 - get result for challenge input part 1" do
-#    File.read!("./inputs/day_eight/challenge_input.txt")
-#    |> DayEight.find_sum_of_small_folders()
-#    |> IO.inspect(label: "Day 08 - Part 01: result for challenge input part 1 is")
-#  end
-#
+  test "Day 08 - get result for challenge input part 1" do
+    File.read!("./inputs/day_eight/challenge_input.txt")
+    |> DayEight.get_number_of_visible_trees()
+    |> IO.inspect(label: "Day 08 - Part 01: result for challenge input part 1 is")
+  end
+
 #  test "Day 08 Part 2 - for test input total size of that directory to be deleted is" do
 #    assert File.read!("./inputs/day_eight/test_input.txt")
 #           |> DayEight.find_size_of_smallest_directory_to_delete() == 24_933_642
