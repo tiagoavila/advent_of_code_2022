1ac2352ead54d425b3c660387ec837799b94c165
Tiago Avila <tiago.avila09@gmail.com>
2023-01-01 14:37:57 -0300

    day 9 part 1

--- lib/day_nine.ex
+++ lib/day_nine.ex
@@ -1,50 +1,66 @@
 defmodule DayNine do
   # == Initial State ==
-  #   012345
-  # 0 ......
-  # 1 ......
-  # 2 ......
-  # 3 ......
-  # 4 H.....  (H covers T, s)
-  # s -> row: 4, column: 0
-  @start_position {4, 0}
+  # ^
+  # | 0 ......
+  # | 1 ......
+  # | 2 ......
+  # | 3 ......
+  # | 4 H.....  (H covers T, s)
+  # y   012345
+  #   x -------------->
+  #
+  # s -> x: 0, y: 4
+  @start_position {0, 4}
 
-  defstruct head_position: @start_position, tail_position: @start_position, visited_positions: MapSet.new([@start_position])
+  defstruct head_position: @start_position,
+            tail_position: @start_position,
+            visited_positions: MapSet.new([@start_position])
 
   def get_positions_tail_visited(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&(&1 |> String.split(" ", trim: true) |> List.to_tuple()))
     |> Enum.reduce(%DayNine{}, &process_instruction/2)
+    |> then(fn %DayNine{visited_positions: visited} -> MapSet.size(visited) end)
   end
 
   defp process_instruction({dir, moves_number}, acc) do
-    range = 1..String.to_integer(moves_number) |> IO.inspect
+    Enum.reduce(1..String.to_integer(moves_number), acc, fn _, acc ->
+      head_position = move_head(dir, acc.head_position)
+      tail_position = move_tail(head_position, acc.tail_position)
+      visited_positions = MapSet.put(acc.visited_positions, tail_position)
 
-    acc
-
-#    range
-#    |> Enum.reduce
+      %DayNine{
+        head_position: head_position,
+        tail_position: tail_position,
+        visited_positions: visited_positions
+      }
+    end)
   end
 
-#  defp process_instruction({"R", moves_number}, acc) do
-#    moves_number
-#    |> String.to_integer()
-#  end
-#
-#  defp process_instruction({"U", moves_number}, acc) do
-#    acc
-#  end
-#
-#  defp process_instruction({"L", moves_number}, acc) do
-#    acc
-#  end
-#
-#  defp process_instruction({"D", moves_number}, acc) do
-#    acc
-#  end
+  defp move_head("R", {x, y}), do: {x + 1, y}
+  defp move_head("L", {x, y}), do: {x - 1, y}
+  defp move_head("U", {x, y}), do: {x, y + 1}
+  defp move_head("D", {x, y}), do: {x, y - 1}
 
-  defp move_head() do
+  # Head and Tail at same position: do nothing
+  defp move_tail({x, y}, {x, y}), do: {x, y}
 
+  # Head and Tail at adjacent positions: do nothing
+  defp move_tail({hx, hy}, {tx, ty}) when abs(hx - tx) <= 1 and abs(hy - ty) <= 1, do: {tx, ty}
+
+  # Head and Tail at same col: move
+  defp move_tail({x, hy}, {x, ty}) when hy > ty, do: {x, ty + 1}
+  defp move_tail({x, hy}, {x, ty}) when hy < ty, do: {x, ty - 1}
+
+  # Head and Tail at same row: move
+  defp move_tail({hx, y}, {tx, y}) when hx > tx, do: {tx + 1, y}
+  defp move_tail({hx, y}, {tx, y}) when hx < tx, do: {tx - 1, y}
+
+  # Head and Tail at diagonal positions
+  defp move_tail({hx, hy}, {tx, ty}) do
+    dx = if hx - tx > 0, do: 1, else: -1
+    dy = if hy - ty > 0, do: 1, else: -1
+    {tx + dx, ty + dy}
   end
 end
--- test/day_nine_test.exs
+++ test/day_nine_test.exs
@@ -7,11 +7,11 @@ defmodule DayNineTest do
            |> DayNine.get_positions_tail_visited() == 13
   end
 
-  #  test "Day 09 - get result for challenge input part 1" do
-  #    File.read!("./inputs/day_nine/challenge_input.txt")
-  #    |> DayNine.get_number_of_visible_trees()
-  #    |> IO.inspect(label: "Day 09 - Part 01: result for challenge input part 1 is")
-  #  end
+    test "Day 09 - get result for challenge input part 1" do
+      File.read!("./inputs/day_nine/challenge_input.txt")
+      |> DayNine.get_positions_tail_visited()
+      |> IO.inspect(label: "Day 09 - Part 01: result for challenge input part 1 is")
+    end
 
 #  test "Day 09 Part 2 - for test input highest scenic score is 8" do
 #    assert File.read!("./inputs/day_nine/test_input.txt")
