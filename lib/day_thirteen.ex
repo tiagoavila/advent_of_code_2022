58b909ba1c0c2ff0084ed55e59292c321194f700
Tiago Avila <tiago.avila09@gmail.com>
2023-02-01 19:38:11 -0300

    Day 12 + 1 part 1 finished

--- lib/day_thirteen.ex
+++ lib/day_thirteen.ex
@@ -1,63 +1,71 @@
 defmodule DayThirteen do
-  defmodule PairData do
-    defstruct [:pair_tuples, :left_count, :right_count]
+  def get_sum_of_pairs_with_right_order(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.with_index(1)
+    |> Enum.reduce(0, fn {pair, pair_number}, acc ->
+      if check_pair_is_in_right_order?(pair) == true, do: acc + pair_number, else: acc
+    end)
   end
 
   def check_pair_is_in_right_order?(input) do
-    %PairData{pair_tuples: pair_tuples, left_count: left_count, right_count: right_count} =
+    [left, right] =
       input
       |> parse_line_to_pair_data()
 
-    pair_tuples
-    |> Enum.reduce_while(:continue, fn {left, right}, _acc ->
-      pair_is_in_right_order?(left, right)
-    end)
-    |> IO.inspect(label: "test")
+    check_order_in_left_and_right_lists(left, right)
+    |> then(fn {_, value} -> value end)
+  end
 
-    # |> then(fn {left_pair, right_pair} -> pair_is_in_right_order?(left_pair, right_pair) end)
+  defp check_order_in_left_and_right_lists(left, right) do
+    Enum.zip(left, right)
+    |> Enum.reduce_while(:continue, fn {left_item, right_item}, _acc ->
+      check_pair?(left_item, right_item)
+    end)
+    |> then(fn value -> check_for_remaining_items(value, length(left), length(right)) end)
   end
 
   defp parse_line_to_pair_data(line) do
-    [left, right] =
-      line
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn line ->
-        {pair_item, _} = Code.eval_string(line)
-        pair_item
-      end)
-
-    %PairData{
-      pair_tuples: Enum.zip(left, right),
-      left_count: length(left),
-      right_count: length(right)
-    }
+    line
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      {pair_item, _} = Code.eval_string(line)
+      pair_item
+    end)
   end
 
-  # defp pair_is_in_right_order?([[left_head] | left_tail], [[right_head] | right_tail]) do
-  # 	IO.puts("Heads are both lists. left_head: #{left_head} - right_head: #{right_head}")
-  # 	pair_is_in_right_order?(left_tail, right_tail)
-  # end
+  # both values are integer and have the same value, continue checking
+  defp check_pair?(value, value) when is_integer(value), do: {:cont, :continue}
 
-  defp pair_is_in_right_order?(value, value) when is_integer(value) do
-    # IO.puts("Heads are equal. head: #{value}")
-    {:cont, :continue}
-  end
+  # left is greater than right, not in right order
+  defp check_pair?(left, right) when is_integer(left) and is_integer(right) and left > right,
+    do: {:halt, false}
 
-  defp pair_is_in_right_order?(left, right)
-       when is_integer(left) and is_integer(right) and left > right do
-    # IO.puts("left head is greater - left_head: #{left_head} - right_head: #{right_head}")
-    {:halt, false}
-  end
+  # left is smaller than right, is in right order
+  defp check_pair?(left, right) when is_integer(left) and is_integer(right) and left < right,
+    do: {:halt, true}
 
-  defp pair_is_in_right_order?(left, right)
-       when is_integer(left) and is_integer(right) and left < right do
-    # IO.puts("left head is smaller - left_head: #{left_head} - right_head: #{right_head}")
-    {:halt, true}
-  end
+  # both are lists, continue checking
+  defp check_pair?(left, right) when is_list(left) and is_list(right),
+    do: check_order_in_left_and_right_lists(left, right)
 
-  defp pair_is_in_right_order?(left, right)
-       when is_list(left) and is_list(right) do
-    # IO.puts("left head is greater - left_head: #{left_head} - right_head: #{right_head}")
-    {:halt, false}
-  end
+  # right is integer, parse it to list and continue checking
+  defp check_pair?(left, right) when is_list(left) and is_integer(right),
+    do: check_order_in_left_and_right_lists(left, [right])
+
+  # left is integer, parse it to list and continue checking
+  defp check_pair?(left, right) when is_integer(left) and is_list(right),
+    do: check_order_in_left_and_right_lists([left], right)
+
+  # lists have the same length after running reduce_while and returned :continue, continue checking
+  defp check_for_remaining_items(:continue, count, count), do: {:cont, :continue}
+
+  # lists don't have the same length after running reduce_while and returned :continue,
+  # If the left list runs out of items first, the inputs are in the right order.
+  # If the right list runs out of items first, the inputs are not in the right order
+  defp check_for_remaining_items(:continue, left_count, right_count),
+    do: {:halt, left_count < right_count}
+
+  # check_pair found if order is correct or not, so just return the value with :halt to stop the reduce_while
+  defp check_for_remaining_items(value, _, _), do: {:halt, value}
 end
--- test/day_thirteen_test.exs
+++ test/day_thirteen_test.exs
@@ -18,11 +18,64 @@ defmodule DayThirteenTest do
            |> DayThirteen.check_pair_is_in_right_order?() == true
   end
 
-  # test "Day 13 - get result for challenge input part 1" do
-  #   File.read!("./inputs/day_thirteen/challenge_input.txt")
-  #   |> DayThirteen.get_step_number_of_smallest_path()
-  #   |> IO.inspect(label: "Day 13 - Part 01: result for challenge input part 1 is")
-  # end
+  test "Day 13 - testing pair 3: it isn't in right order" do
+    assert """
+           [9]
+           [[8,7,6]]
+           """
+           |> DayThirteen.check_pair_is_in_right_order?() == false
+  end
+
+  test "Day 13 - testing pair 4: it's in right order" do
+    assert """
+           [[4,4],4,4]
+           [[4,4],4,4,4]
+           """
+           |> DayThirteen.check_pair_is_in_right_order?() == true
+  end
+
+  test "Day 13 - testing pair 5: it isn't in right order" do
+    assert """
+           [7,7,7,7]
+           [7,7,7]
+           """
+           |> DayThirteen.check_pair_is_in_right_order?() == false
+  end
+
+  test "Day 13 - testing pair 6: it's in right order" do
+    assert """
+           []
+           [3]
+           """
+           |> DayThirteen.check_pair_is_in_right_order?() == true
+  end
+
+  test "Day 13 - testing pair 7: it isn't in right order" do
+    assert """
+           [[[]]]
+           [[]]
+           """
+           |> DayThirteen.check_pair_is_in_right_order?() == false
+  end
+
+  test "Day 13 - testing pair 8: it isn't in right order" do
+    assert """
+           [1,[2,[3,[4,[5,6,7]]]],8,9]
+           [1,[2,[3,[4,[5,6,0]]]],8,9]
+           """
+           |> DayThirteen.check_pair_is_in_right_order?() == false
+  end
+
+  test "Day 13 -  for test input sum of the indices of pairs with right orders is 13" do
+    assert File.read!("./inputs/day_thirteen/test_input.txt")
+           |> DayThirteen.get_sum_of_pairs_with_right_order() == 13
+  end
+
+  test "Day 13 - get result for challenge input part 1" do
+    File.read!("./inputs/day_thirteen/challenge_input.txt")
+    |> DayThirteen.get_sum_of_pairs_with_right_order()
+    |> IO.inspect(label: "Day 13 - Part 01: result for challenge input part 1 is")
+  end
 
   #  test "Day 13 Part 2 - for test input fewest steps required to move
   #  starting from any square with elevation a to the location that
